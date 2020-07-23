9f31d49d3a5b7e8e19c75da06b235df651e9fa8d
User <dmitryhdl@gmail.com>
2020-07-23 22:10:42 +0300

    Изменение в логине и пароле

--- app/src/main/java/com/example/myapplication/AuthActivity.java
+++ app/src/main/java/com/example/myapplication/AuthActivity.java
@@ -1,11 +1,17 @@
 package com.example.myapplication;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -18,16 +24,41 @@ public class AuthActivity extends AppCompatActivity {
     private View.OnClickListener monEnterClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-        // todo Обработка нажатия по кнопке
+            if (isEmailValid() && isPasswordValid()) {
+            // переход в приложение
+            }else{
+                ShowMessage(R.string.login_input_error);
+            }
         }
-    };
+};
+// Метод проверки Логина на корректность
+    private boolean isEmailValid() {
+        return !TextUtils.isEmpty(mLogin.getText())
+                && Patterns.EMAIL_ADDRESS.matcher(mLogin.getText()).matches();
+
+    }
 
     private View.OnClickListener monRegisterClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            // todo Обработка нажатия по кнопке
+
         }
     };
+    // Метод проверки Пароля на корректность
+    private boolean isPasswordValid() {
+        return !TextUtils.isEmpty(mPassword.getText());
+
+    }
+
+    /**
+     * Метод исключения
+     *
+      */
+
+    private void ShowMessage(@StringRes int string) {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show();
+    }
+
 
     // Отображение на экране телефона метод onCreate
     @Override
